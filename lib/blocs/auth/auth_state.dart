1acc1bcb57fc9aeae1eb3c734ebb3efa044f18c0
YoungJin Lim <caribjin@gmail.com>
2021-04-26 04:17:59 +0900

    auth bloc, event, state 로직 추가.

--- lib/blocs/auth/auth_state.dart
+++ lib/blocs/auth/auth_state.dart
@@ -1,10 +1,29 @@
 part of 'auth_bloc.dart';
 
-abstract class AuthState extends Equatable {
-  const AuthState();
+enum AuthStatus {
+  unknown,
+  authenticated,
+  unauthenticated,
 }
 
-class AuthInitial extends AuthState {
+class AuthState extends Equatable {
+  final auth.User? user;
+  final AuthStatus status;
+
+  const AuthState({
+    this.user,
+    this.status = AuthStatus.unknown,
+  });
+
+  factory AuthState.unknown() => const AuthState();
+
+  factory AuthState.authenticated({required auth.User user}) => AuthState(user: user, status: AuthStatus.authenticated);
+
+  factory AuthState.unauthenticated() => const AuthState(status: AuthStatus.unauthenticated);
+
   @override
-  List<Object> get props => [];
+  bool? get stringify => true;
+
+  @override
+  List<Object?> get props => [];
 }
