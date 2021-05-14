d84abe34a5f928b5178a849b245d6191d224d705
YoungJin Lim <caribjin@gmail.com>
2021-05-14 23:02:06 +0900

    profile screen에 생성된 사용자의 post를 grid, list 탭으로 출력.

--- lib/screens/profile/bloc/profile_bloc.dart
+++ lib/screens/profile/bloc/profile_bloc.dart
@@ -11,17 +11,32 @@ part 'profile_state.dart';
 
 class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
   final UserRepository _userRepository;
+  final PostRepository _postRepository;
   final AuthBloc _authBloc;
 
-  ProfileBloc({required UserRepository userRepository, required AuthBloc authBloc})
+  StreamSubscription<List<Future<Post?>>>? _postsSubscription;
+
+  ProfileBloc(
+      {required UserRepository userRepository, required PostRepository postRepository, required AuthBloc authBloc})
       : _userRepository = userRepository,
+        _postRepository = postRepository,
         _authBloc = authBloc,
         super(ProfileState.initial());
+
+  @override
+  Future<void> close() {
+    _postsSubscription?.cancel();
+    return super.close();
+  }
 
   @override
   Stream<ProfileState> mapEventToState(ProfileEvent event) async* {
     if (event is ProfileLoadUser) {
       yield* _mapProfileLoadUserToState(event);
+    } else if (event is ProfileToggleGridView) {
+      yield* _mapProfileToggleGridViewToState(event);
+    } else if (event is ProfileUpdatePosts) {
+      yield* _mapProfileUpdatePostsToState(event);
     }
   }
 
@@ -31,6 +46,12 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
     try {
       final user = await _userRepository.getUserWithId(userId: event.userId);
       final isCurrentUser = _authBloc.state.user!.uid == event.userId;
+
+      _postsSubscription?.cancel();
+      _postsSubscription = _postRepository.getUserPosts(userId: event.userId).listen((posts) async {
+        final allPosts = await Future.wait(posts);
+        add(ProfileUpdatePosts(posts: allPosts));
+      });
 
       yield state.copyWith(
         user: user,
@@ -43,5 +64,13 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
         failure: const Failure(message: 'We were unable to load this profile.'),
       );
     }
+  }
+
+  Stream<ProfileState> _mapProfileToggleGridViewToState(ProfileToggleGridView event) async* {
+    yield state.copyWith(isGridView: event.isGridView);
+  }
+
+  Stream<ProfileState> _mapProfileUpdatePostsToState(ProfileUpdatePosts event) async* {
+    yield state.copyWith(posts: event.posts);
   }
 }
--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:finsta/blocs/blocs.dart';
 import 'package:finsta/screens/profile/bloc/profile_bloc.dart';
 import 'package:finsta/screens/profile/widgets/widgets.dart';
@@ -5,8 +6,27 @@ import 'package:finsta/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   static const String routeName = '/profile';
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,40 +52,106 @@ class ProfileScreen extends StatelessWidget {
                 ),
             ],
           ),
-          body: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(24, 32, 24, 0),
-                      child: Row(
-                        children: [
-                          UserProfileImage(
-                            radius: 40,
-                            profileImageUrl: state.user.profileImageUrl,
-                          ),
-                          ProfileStats(
-                            isCurrentUser: state.isCurrentUser,
-                            isFollowing: state.isFollowing,
-                            posts: 0, // state.post.length,
-                            followers: state.user.followers,
-                            following: state.user.following,
-                          ),
-                        ],
+          body: RefreshIndicator(
+            onRefresh: () async {
+              context.read<ProfileBloc>().add(ProfileLoadUser(userId: state.user.id));
+            },
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(24, 32, 24, 0),
+                        child: Row(
+                          children: [
+                            UserProfileImage(
+                              radius: 40,
+                              profileImageUrl: state.user.profileImageUrl,
+                            ),
+                            ProfileStats(
+                              isCurrentUser: state.isCurrentUser,
+                              isFollowing: state.isFollowing,
+                              posts: 0,
+                              // state.post.length,
+                              followers: state.user.followers,
+                              following: state.user.following,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                      child: ProfileInfo(
-                        username: state.user.username,
-                        bio: state.user.bio,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                        child: ProfileInfo(
+                          username: state.user.username,
+                          bio: state.user.bio,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                SliverToBoxAdapter(
+                  child: TabBar(
+                    controller: _tabController,
+                    labelColor: Theme.of(context).primaryColor,
+                    unselectedLabelColor: Colors.grey,
+                    // indicatorWeight: 3,
+                    tabs: [
+                      Tab(
+                        icon: Icon(Icons.grid_on),
+                      ),
+                      Tab(
+                        icon: Icon(Icons.list),
+                      ),
+                    ],
+                    onTap: (index) {
+                      context.read<ProfileBloc>().add(ProfileToggleGridView(isGridView: index == 0));
+                    },
+                  ),
+                ),
+                state.isGridView
+                    ? SliverGrid(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          mainAxisSpacing: 2,
+                          crossAxisSpacing: 2,
+                        ),
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                            if (state.posts.isEmpty) return null;
+
+                            final post = state.posts[index];
+
+                            return GestureDetector(
+                              child: Container(
+                                child: CachedNetworkImage(
+                                  imageUrl: post!.imageUrl,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              onTap: () {},
+                            );
+                          },
+                          childCount: state.posts.length,
+                        ),
+                      )
+                    : SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                            final post = state.posts[index];
+
+                            return Container(
+                              margin: EdgeInsets.all(10),
+                              height: 100.0,
+                              width: double.infinity,
+                              color: Colors.red,
+                            );
+                          },
+                          childCount: state.posts.length,
+                        ),
+                      ),
+              ],
+            ),
           ),
         );
       },
